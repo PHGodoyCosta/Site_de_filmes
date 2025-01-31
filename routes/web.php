8ba8a68be2dc11273337696cab8c0131e1443895
phgodoycosta <phgodoycosta@gmail.com>
2025-01-31 17:35:49 -0400

    Adicionando rotas principais para o player

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Filmes;
+use App\Http\Controllers\FilmeController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get("/filme/{filme}", function (string $filmeHash) {
+    $filme = Filmes::where("hash", $filmeHash)->firstOrFail();
+
+    return view("filme", ["filme" => $filme]);
+});
+
+Route::get("/filme", FilmeController::class);
